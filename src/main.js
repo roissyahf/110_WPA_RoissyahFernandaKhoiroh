b79309c401045b7508180548abfdaea1802040b4
roissyahf <roissyahfk@gmail.com>
2022-08-07 19:36:51 +0700

    delete css and js files in src folder

--- src/main.js
+++ src/main.js
@@ -2,10 +2,5 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-import '../src/assets/css/owl.carousel.min.css'
-import '../src/assets/css/owl.theme.default.css'
-
-import '../src/assets/js/owl.carousel'
-import '../src/assets/js/owl.carousel'
 
 createApp(App).use(router).mount('#app')
